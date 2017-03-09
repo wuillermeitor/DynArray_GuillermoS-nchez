4d91b539e59fafd5d6da9fd250d50327a3fdf503
Guillermo Sánchez ENTI <guillermosanchezsanchez@enti.cat>
2017-03-09 12:54:27 +0100

    ver 1.2

--- DynArray/DynArray.cc
+++ DynArray/DynArray.cc
@@ -1,22 +1,40 @@
 #include "DynArray.hh"
 #include <iostream>
 
+//constructores
 DynArray::DynArray(void) {
-	m_size = 0;
 	m_capacity = DYN_ARRAY_DEFAULT_SIZE;
+	m_size = 0;
 	m_data = new int[m_capacity];
+}
+
+/*DynArray::DynArray(void):
+	m_capacity(DYN_ARRAY_DEFAULT_SIZE),
+	m_size(0),
+	m_data(new int[m_capacity])
+{
+}
+*/
+
+DynArray::DynArray(size_t size, const int &value) {
+	m_capacity = size;
+	m_size = size;
+	m_data = new int[size];
+	
+	fill(begin(), end(), value);
 }
 
 DynArray::DynArray(int *arr, size_t size) {
 	m_capacity = size;
-	m_data = new int[m_capacity];
 	m_size = size;
+	m_data = new int[size];
 
 	for (int i = 0; i < size; i++) {
 		m_data[i] = arr[i];
 	}
 }
 
+//destructor
 DynArray::~DynArray() {
 	delete[]m_data;
 }
@@ -71,7 +89,51 @@ bool operator!= (const DynArray& lhs, const DynArray& rhs) {
 }
 
 bool operator<  (const DynArray& lhs, const DynArray& rhs) {
+	int i = 0;
+	int j = 0;
+	while (i < lhs.m_size) {
+		i++;
+	}
+	while (j < rhs.m_size) {
+		j++;
+	}
+	return i < j;
+}
 
+bool operator<=  (const DynArray& lhs, const DynArray& rhs) {
+	int i = 0;
+	int j = 0;
+	while (i < lhs.m_size) {
+		i++;
+	}
+	while (j < rhs.m_size) {
+		j++;
+	}
+	return i <= j;
+}
+
+bool operator>  (const DynArray& lhs, const DynArray& rhs) {
+	int i = 0;
+	int j = 0;
+	while (i < lhs.m_size) {
+		i++;
+	}
+	while (j < rhs.m_size) {
+		j++;
+	}
+	return i > j;
+}
+
+bool operator>=  (const DynArray& lhs, const DynArray& rhs) {
+	int i = 0;
+	int j = 0;
+	while (i < lhs.m_size) {
+		i++;
+	}
+	while (j < rhs.m_size) {
+		j++;
+	}
+	return i >= j;
 }
 
 std::ostream& operator<< (std::ostream &out, const DynArray &x) {
--- DynArray/main.cc
+++ DynArray/main.cc
@@ -1,2 +1,10 @@
 #include "DynArray.hh"
 #include <iostream>
+
+void main() {
+	DynArray d1;
+	DynArray d2(5, 10);
+	d1.push(20);
+	//1. Verificar capacidad.
+	//2. si no tengo sitio para ponerla...
+}
